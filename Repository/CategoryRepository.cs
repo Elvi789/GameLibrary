bbf436b5263b3a80a4fe172ce114f1027fd665cd
Albi Idrizi <albiidrizi27@gmail.com>
2024-03-09 14:08:22 +0100

    Deleted commented code

--- Repository/CategoryRepository.cs
+++ Repository/CategoryRepository.cs
@@ -10,34 +10,7 @@ namespace GameLibrary.Repository
         private readonly ApplicationDbContext _context;
         public CategoryRepository(ApplicationDbContext context) : base(context)  { _context = context; } 
 
-        //public async Task<List<Category>> GetAllCategoriesAsync()
-        //{
-        //    return await _context.Categories.ToListAsync();
-        //}
-
-        //public async Task<Category> GetCategoryByIdAsync(int id)
-        //{
-        //    return await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-        //}
-
-        //public async Task CreateCategory(Category category)
-        //{
-        //    await _context.Categories.AddAsync(category);
-        //    await _context.SaveChangesAsync();
-        //}
-
-        //public async Task DeleteCategory(Category category)
-        //{
-        //    _context.Categories.Remove(category);
-        //    await _context.SaveChangesAsync();
-        //}
-
-        //public async Task EditCategory(Category category)
-        //{
-        //    _context.Categories.Update(category);
-        //    await _context.SaveChangesAsync();
-        //}
-
+       
 
 
         // Nese do te na duhet ndonje metode e gatshme dhe specifike rreth category do ta shtojme ne kete file, ne te kundert te gjitha 
